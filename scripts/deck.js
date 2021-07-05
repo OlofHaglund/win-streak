31036855ef3cd872bad0c5f1d822f2c9d1ea47d0
Olof Haglund <olof@olofhaglund.name>
2021-07-05 14:31:36 +0200

    Added revisions do deck.

--- scripts/deck.js
+++ scripts/deck.js
@@ -16,6 +16,11 @@ function populate_deck(deck){
     winrate = (deck.wins/total_games)*100;
     html = html.replace("{5}", `${winrate.toFixed(2)}%`)
   }
+  html = html.replace("{t_winrate}", `${get_total_winrate(deck.id)}%`)
+
+  let revision = build_revisions(deck);
+  html = html.replace("{revision}", revision)
+
   let doc       = new DOMParser().parseFromString(html, 'text/html').body.childNodes;
   let deck_list = document.getElementById('decklist');
   deck_list.append(doc[0]);
@@ -25,13 +30,35 @@ function populate_deck(deck){
   });
 }
 
+function build_revisions(deck) {
+  let revisions = Object.keys(deck.revisions).sort((a,b) => b - a );
+  console.log(revisions);
+  let end = revisions.length > 5 ? 5 : revisions.length;
+  let html = ""
+
+  for (i = 0; i < end; i++) {
+    htm = revision_html.replace("{wins}", deck.revisions[revisions[i]].wins);
+    htm = htm.replace("{loss}", deck.revisions[revisions[i]].loss);
+
+    total = deck.revisions[revisions[i]].wins + deck.revisions[revisions[i]].loss;
+    percentage = (deck.revisions[revisions[i]].wins / total) * 100;
+    htm = htm.replace("{winrate}",`${percentage.toFixed("2")}%`);
+
+    html += htm;
+  }
+  return html;
+}
+
 function create_new_deck(){
   let id  = decks.length;
   let obj = {
-    id:     id,
-    name:   "",
-    wins:   0,
-    loss:   0
+    id:         id,
+    revisions:  {},
+    name:       "",
+    wins:       0,
+    loss:       0,
+    start_date: Date.now(),
+    version:    1,
   }
   decks.push(obj);
   populate_deck(obj);
@@ -78,6 +105,51 @@ function update_winrate(id){
   form_id = "deck-" + id;
   text = document.getElementById(form_id).querySelector(".winrate");
   total_games = decks[id].wins + decks[id].loss;
-  rate = (decks[id].wins/total_games)*100
-  text.innerHTML = `Winrate: ${rate.toFixed(2)}%`;
+  if (total_games > 0) {
+    rate = (decks[id].wins/total_games)*100;
+    text.innerHTML = `Winrate: ${rate.toFixed(2)}%`;
+  }
+  else {
+    text.innerHTML = `Winrate: -`;
+  }
+}
+
+function new_revision(id){
+  decks[id].revisions[Date.now()] ={
+    wins: decks[id].wins,
+    loss: decks[id].loss
+  }
+  decks[id].wins = 0;
+  decks[id].loss = 0;
+
+  form_id = "deck-" + id;
+  button = document.getElementById(form_id).querySelector(".win");
+  button.innerHTML = decks[id].wins + " Wins";
+
+  form_id = "deck-" + id;
+  button = document.getElementById(form_id).querySelector(".loss");
+  button.innerHTML = decks[id].loss + " Losses";
+
+  games_played(id);
+  update_winrate(id);
+
+  localStorage.setItem('decks', JSON.stringify(decks));
+
+  populate_deck(decks[id]);
+}
+
+function get_total_winrate(id){
+  let total_wins = decks[id].wins;
+  let total_loss = decks[id].loss;
+  for(let i in decks[id].revisions){
+    total_wins += decks[id].revisions[i].wins;
+    total_loss += decks[id].revisions[i].loss;
+  }
+  let total = total_wins + total_loss;
+  if(total > 0){
+    return ((total_wins/total)*100).toFixed(2);
+  }
+  else{
+    return 0;
+  }
 }
--- scripts/declartions.js
+++ scripts/declartions.js
@@ -1,5 +1,5 @@
 let deck_html = `
-<div class="container-fluid">
+<div class="container-fluid mb-4">
   <div class="row scrolling-wrapper flex-row flex-nowrap">
     <div class="col-3">
       <form class="deck" id="deck-{0}">
@@ -8,7 +8,7 @@ let deck_html = `
           <label for="deck_name" class="form-label">Name of the deck</label>
           <input type="text" class="form-control name" name="deck_name" placeholder="Decks name" value="{1}">
         </div>
-        <div class="row">
+        <div class="row mb-2">
           <div class="col">
             <button type="button" class="btn btn-success win" onclick="win({0})">{2} Wins</button>
           </div>
@@ -16,14 +16,42 @@ let deck_html = `
             <button type="button" class="btn btn-danger loss" onclick="loss({0})">{3} Losses</button>
           </div>
         </div>
-        <span class="games">Games played: {4}</span>
-        <br>
-        <span class="winrate">Winrate: {5}</span>
+        <div class="row">
+          <div class="col">
+            <span class="games">Games played: {4}</span>
+            <br>
+            <span class="winrate">Winrate: {5}</span>
+          </div>
+          <div class="col">
+            <button type="button" class="btn btn-primary revision" onclick="new_revision({0})"> New Revision</button>
+          </div>
+        </div>
       </form>
     </div>
-    <div class="col-8">
-      Area dedicated for handling history
+    <div class="col-9">
+      <div class="row">
+        <div class="col-2">
+          <span class="total_winrate">Total winrate: {t_winrate}</span>
+        </div>
+
+        {revision}
+        </div>
+      </div>
     </div>
+  </div>
+</div>
+`;
+
+let revision_html = `
+<div class="col-2">
+  <div class="row">
+   Winrate: {winrate}
+  </div>
+  <div class="row">
+   Wins: {wins}
+  </div>
+  <div class="row">
+   Loss: {loss}
   </div>
 </div>
 `;
